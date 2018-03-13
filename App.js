d8c005087178ce1abb5da892538e68a2705f2807
Rendita Septiadi <36871435+rendysptd@users.noreply.github.com>
2018-03-14 00:43:31 +0800

    Update App.js

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.containerMain}>
         <Header headerText="Header"/>
-        <Sales />
+        <MainApp/>
         <Footer footerText="Footer"/>
       </View>
     );
